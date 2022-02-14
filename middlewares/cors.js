0b2a8feb00076337c4be9c97085a693f5f72a34d
artemrgrs <artemrgrs@yandex.ru>
2022-02-14 23:53:55 +0300

    cors6

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -1,18 +1,20 @@
 const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
 const allowedCors = [
-  'http://localhost:3000',
   'http://artemrgrs.nomoredomains.work',
   'https://artemrgrs.nomoredomains.work',
+  'localhost:3000',
 ];
 
-module.exports = (req, res, next) => {
+const cors = (req, res, next) => {
   const { origin } = req.headers;
   const { method } = req;
   const reqHeaders = req.headers['access-control-request-headers'];
 
   if (allowedCors.includes(origin)) {
     res.header('Access-Control-Allow-Origin', origin);
+    res.header('Access-Control-Allow-Credentials', true);
   }
+
   if (method === 'OPTIONS') {
     res.header('Access-Control-Allow-Headers', reqHeaders);
     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
@@ -21,3 +23,5 @@ module.exports = (req, res, next) => {
 
   return next();
 };
+
+module.exports = cors;
