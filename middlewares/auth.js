d74e7aa7e3dcc345ec08694799df1c759f7c9143
artemrgrs <artemrgrs@yandex.ru>
2022-02-16 00:04:12 +0300

    auth

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,16 +1,16 @@
 const jwt = require('jsonwebtoken');
 const UnauthorizedError = require('../errors/UnauthorizedError');
 
-const { JWT_SECRET } = process.env;
+const { NODE_ENV, JWT_SECRET } = process.env;
 
 module.exports = (req, res, next) => {
   const token = req.cookies.jwt;
   let payload;
 
   try {
-    payload = jwt.verify(token, JWT_SECRET);
+    payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret');
   } catch (err) {
-    throw new UnauthorizedError(token);
+    throw new UnauthorizedError('Необходима авторизацияzzzzzzz');
   }
 
   req.user = payload;
